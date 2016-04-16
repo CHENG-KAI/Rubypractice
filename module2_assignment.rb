0bb90c83bf8d7fad1a3e302096178be5f2dc2fc5
CHENG-KAI <k20052@yahoo.com.tw>
2016-04-15 21:37:30 -0700

    Fix

--- module2_assignment.rb
+++ module2_assignment.rb
@@ -32,51 +32,44 @@ class Solution
         @LineAnalyzers = Array.new
         @Line_numbers = Array.new
         @max_num=[]
-        #counter = 1
         File.foreach('test.txt') do |line|
             @LineAnalyzers = line.chomp
             @analyzers.push(@LineAnalyzers)
-            #@Line_numbers.push(counter)
-            #counter=counter+1
+        
 
         end
-
-
             
-            @analyzers.each do |element|
-                cal_high_freq =  LineAnalyzer.new(element,0)
-                @max_frequency = cal_high_freq.calculate_word_frequency
-                @max_num.push(@max_frequency) 
+        @analyzers.each do |element|
+            cal_high_freq =  LineAnalyzer.new(element,0)
+            @max_frequency = cal_high_freq.calculate_word_frequency
+            @max_num.push(@max_frequency)
                 
                 
-            end     
-        #p @analyzers, @Line_numbers, @max_frequency ,@max_num
-         #p @LineAnalyzers
-         #p analyzers
-         #p @max_frequency
-         #p @max_num
-         
-         
+        end     
+          
+        # @wang=@max_num 
+        # @lin = Hash[@wang.map{ |key| [key.pop, key] }]
+        # @op = @lin.select {|k,v| v == @lin.values.max }
+        # @equn = @op.keys.flatten(1)
+                  
 
     end
+
 
     def calculate_line_with_highest_frequency()    
         analyze_file
         @highest_count_words_across_lines=Array.new
         @highest_count_across_lines=Array.new
-
+        
         @max_num.each do |core|
              
              @highest_count_words_across_lines.push(core.pop)
              @highest_count_across_lines.push(core.shift)
              
         end
-
+         
+        
         @highest_count_across_lines=@highest_count_across_lines.max
-        
-        
-        
-        
         
 
     end 
@@ -84,8 +77,6 @@ class Solution
     def print_highest_word_frequency_across_lines()
         calculate_line_with_highest_frequency
         puts "The following words have the highest word frequency per line:"
-        
-        #p @highest_count_words_across_lines
         count = 1
         @highest_count_words_across_lines.each do |ulines|
         puts "#{ulines} (appears in line #{count})" 
@@ -95,29 +86,8 @@ class Solution
     end
 
 
-
-
-
 end
 
-abc = []
-counter = 0 
-File.foreach('test.txt') do |line| 
-    @LineAnalyz = line.split    
-    abc.push(@LineAnalyz)
-    counter +=1
-    end
-
-
-
-
-
-
-#LineAnalyzer1 = LineAnalyzer.new("This is a really really really cool cool you you you",1)
-#p LineAnalyzer1.calculate_word_frequency
 LineAnalyzer2 = Solution.new
-#puts LineAnalyzer2.calculate_line_with_highest_frequency
-#LineAnalyzer2.analyze_file
-#LineAnalyzer2.calculate_line_with_highest_frequency
 LineAnalyzer2.print_highest_word_frequency_across_lines
-#p LineAnalyzer2.highest_count_words_across_lines
+
